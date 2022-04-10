396128203e46896396ceb4512c5179233622fd52
MEHTA SHIVAM <mehtashriv.30@gmail.com>
2022-04-10 11:54:03 +0530

    Integrated Database!

--- src/main/java/com/iot/smartgrid/controller/JwtController.java
+++ src/main/java/com/iot/smartgrid/controller/JwtController.java
@@ -13,12 +13,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class JwtController {
 
     @Autowired
---
+++ src/main/java/com/iot/smartgrid/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.iot.smartgrid.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+public interface UserRepository extends JpaRepository<User,Long>
+{
+
+}
