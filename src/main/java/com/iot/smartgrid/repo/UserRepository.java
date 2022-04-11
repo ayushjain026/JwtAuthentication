e57b16fc87aaf290f6094908affc145129b2ff02
MEHTA SHIVAM <mehtashriv.30@gmail.com>
2022-04-11 15:32:10 +0530

    Working!

--- src/main/java/com/iot/smartgrid/repo/UserRepository.java
+++ src/main/java/com/iot/smartgrid/repo/UserRepository.java
@@ -1,9 +1,9 @@
-package com.iot.smartgrid.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
-
-public interface UserRepository extends JpaRepository<User,Long>
-{
-
-}
+//package com.iot.smartgrid.repo;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.security.core.userdetails.User;
+//
+//public interface UserRepository extends JpaRepository<User,Long>
+//{
+//    public User findByUsername(String username);
+//}
--- src/main/java/com/iot/smartgrid/services/CustomUserDetailsService.java
+++ src/main/java/com/iot/smartgrid/services/CustomUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.iot.smartgrid.services;
 
+//import com.iot.smartgrid.model.CustomUserDetails;
+//import com.iot.smartgrid.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,8 +14,21 @@ import java.util.ArrayList;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+//    @Autowired
+//    private UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException{
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException
+    {
+
+//        final User user = this.userRepository.findByUsername(userName);
+//
+//        if(user==null){
+//            throw new UsernameNotFoundException("User not found");
+//        }
+//        else{
+//            return new CustomUserDetails(user);
+//        }
         if(userName.equals("Ayush")){
             return new User("Ayush", "ayush", new ArrayList<>());
         }
